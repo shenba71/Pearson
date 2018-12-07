4fb278bf00f330c17c976a39fbe1ffe3f774fa5e
Sandeep Menon <sandeep.menon@aspiresys.com>
2018-12-07 15:00:15 +0530

    Provided DashBoard Link

--- src/app/create-report/create-report.component.ts
+++ src/app/create-report/create-report.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var visualize: any;
+declare var $ :any;
 @Component({
   selector: 'app-create-report',
   templateUrl: './create-report.component.html',
@@ -10,6 +12,29 @@ export class CreateReportComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  this.initializeDashBoard();
+  }
+  
+  initializeDashBoard() {
+    visualize({
+    auth: {
+        name: "jasperadmin",
+        password: "jasperadmin",
+        organization: "organization_1"
+    }
+    }, function (v) {
+
+    //render report from provided resource
+    v("#container").dashboard({
+        resource: "/dashboards/Login_Details_DashBoard",
+        error: handleError
+    });
+
+    //show error
+    function handleError(err) {
+        alert(err.message);
+    }
+    });
   }
 
 }
