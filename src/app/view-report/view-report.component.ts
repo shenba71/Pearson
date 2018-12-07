e0f28ba125c0490c5aeb1afafad0ccc609bc8e77
Sandeep Menon <sandeep.menon@aspiresys.com>
2018-12-07 11:25:11 +0530

    Jasper Integration

--- src/app/view-report/view-report.component.ts
+++ src/app/view-report/view-report.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var visualize: any;
+declare var $ :any;
 @Component({
   selector: 'app-view-report',
   templateUrl: './view-report.component.html',
@@ -10,6 +12,46 @@ export class ViewReportComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  this.initializeJasper();
+  }
+  
+  initializeJasper(){
+  visualize({
+  auth: {
+    name: "jasperadmin",
+    password: "jasperadmin"
+
+  }
+}, function(v) {
+  var adv = v.adhocView({
+    resource: "/adhoc/aru/MongoLoginView",
+    //resource: "/public/AdHocViews/MongoLoginView",
+    container: "#container",
+  });
+
+  //render initial view
+  setInitialType();
+
+  function setInitialType() {
+    adv.canvas({
+      type: $("#selected_resource").val()
+    }).run().fail(handleError);
+  }
+
+  $("#selected_resource").change(function() {
+    adv.canvas({
+      type: $("#selected_resource").val()
+    }).run().fail(handleError);
+  });
+
+  function handleError(err) {
+    console.log(err);
+  }
+
+  $('#selected_resource').prop('disabled', false);
+
+});
+  
   }
 
 }
